82f459799110f5b4a41763059ef2f3c39f23c7e9
Амина Кадралинова <Tyuiu.KadralinovaAT@outlook.com>
2025-11-11 00:26:54 +0500

    Task7

--- Tyuiu.KadralinovaAT.Sprint4.Task7.V13.Test/DataServiceTest.cs
+++ Tyuiu.KadralinovaAT.Sprint4.Task7.V13.Test/DataServiceTest.cs
@@ -5,8 +5,16 @@ namespace Tyuiu.KadralinovaAT.Sprint4.Task7.V13.Test
     public sealed class DataServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ValidCalc()
         {
+            int rows = 3;
+            int columns = 3;
+            int[,] mtrx = new int[rows, columns];
+            string str = "159357246";
+            DataService ds = new DataService();
+            int res = ds.Calculate(rows, columns, str);
+            int wait = 3;
+            Assert.AreEqual(wait, res);
         }
     }
 }
--- Tyuiu.KadralinovaAT.Sprint4.Task7.V13/Program.cs
+++ Tyuiu.KadralinovaAT.Sprint4.Task7.V13/Program.cs
@@ -1,1 +1,43 @@
 ﻿using Tyuiu.KadralinovaAT.Sprint4.Task7.V13.Lib;
+int rows = 3;
+int columns = 3;
+int[,] mtrx = new int[rows, columns];
+
+string str = "159357246";
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #4 | Выполнила: Кадралинова А. Т. | ПИНб-25-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #4                                                               *");
+Console.WriteLine("* Тема: Двумерные массивы                                                 *");
+Console.WriteLine("* Задание #7                                                              *");
+Console.WriteLine("* Вариант #13                                                             *");
+Console.WriteLine("* Выполнила: Кадралинова Амина Тастемировна | ПИНб-25-1                   *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* подсчитайте количество четных чисел                                     *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+int index = 0;
+
+Console.WriteLine("\n Массив:");
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write($"{str[index]} \t");
+        index++;
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+int res = ds.Calculate(rows, columns, str);
+Console.WriteLine("Количество четных чисел = " + res);
+Console.ReadKey();
